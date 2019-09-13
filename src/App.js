2a1245f73d298f0d64449d3e19724f51e7e95f39
mevinyanuar <mmevinalifviyanuar@gmail.com>
2019-09-13 15:29:20 +0700

    sign in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, } from "react-router-dom";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import Album from "./components/Album";
