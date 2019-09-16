2173f196521bdbe0a35423a628a52f550d896b3b
mevinyanuar <mmevinalifviyanuar@gmail.com>
2019-09-16 15:44:33 +0700

    link to sign-inand sign up

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -5,13 +5,16 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import {
+  BrowserRouter as Router,
+  Route, Link
+} from "react-router-dom";
 
 function Copyright() {
   return (
@@ -160,7 +163,7 @@ export default function SignUp(props) {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link to="/sign-in" variant="body2">d
                 Already have an account? Sign in
               </Link>
             </Grid>
